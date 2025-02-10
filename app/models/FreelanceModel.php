b15ee7ba2f88b84eb767bf052b5d67a2f8a654a9
smdilyass <smdilyass@gmail.com>
2025-02-10 10:25:43 +0100

    I add FreelanceModel to handle project search functionality

---
+++ app/models/FreelanceModel.php
@@ -0,0 +1,20 @@
+<?php
+namespace app\Models;
+use app\config\Database;
+class FreelanceModel {
+    protected $conn;
+
+
+    public function __construct() {
+        $this->conn = Database::getInstance()->getConnection();
+    }
+
+    public function searchProjectsByName($name, $className) {
+        $stmt = $this->conn->prepare("SELECT * FROM projects WHERE name LIKE ?");
+        $stmt->execute(['%' . $name . '%']);
+        return $stmt->fetchAll(PDO::FETCH_CLASS, $className);
+    }
+}
+
+
+?>
